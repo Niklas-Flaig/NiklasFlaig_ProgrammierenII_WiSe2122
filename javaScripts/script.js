eba5b3b35acf65e1e3d1491ed83c0c150707a823
Niklas-Flaig <niklasjensflaig@gmail.com>
2021-11-08 22:43:29 +0100

    added renderChatSelection() to script.js

---
+++ javaScripts/script.js
@@ -0,0 +1,16 @@
+// a refference on the current UserProfile
+const currentProfile = profiles.find(profile => profile.userID === loggedInProfileID);
+
+console.log(currentProfile);
+renderChatSelection();
+
+
+// Will render the ChatSelection in the sidebar
+function renderChatSelection() {
+  let chatSelectionHtml = ``;
+  // for each chat with a Id, matching a ID from the profiles chat-array, execute .writeChatSelectHtml
+  currentProfile.chatIDs.forEach(currentChatID => {
+    chatSelectionHtml += chats.find(chat => chat.chatID === currentChatID).writeChatSelectHtml()
+  });
+  document.querySelector("#chatSelectSurface").innerHTML = chatSelectionHtml;
+}

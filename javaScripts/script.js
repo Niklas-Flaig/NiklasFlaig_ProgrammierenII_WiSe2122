c511c22a61c64f21f20c678f123fd2407ef0abfb
Niklas-Flaig <niklasjensflaig@gmail.com>
2021-11-09 17:08:13 +0100

    added functionallity to sendMessage_Button

--- javaScripts/script.js
+++ javaScripts/script.js
@@ -1,5 +1,20 @@
+document.querySelector("#sendMessage_Button").addEventListener("click", function () {
+  // get the written Text
+  const messageText = document.querySelector("#textMessage_TextInput").value;
+  // add a new TextMessage to the current Chats history
+  chats.find(chat => chat.chatID === selectedChatID).addToHistory(new TextMessage(loggedInProfileID, messageText));
+  
+  // re-render the chatHistory and clear the input field
+  document.querySelector(`#chatHistory`).innerHTML = chats.find(chat => chat.chatID === selectedChatID).writeHistoryHtml();
+  document.querySelector("#textMessage_TextInput").value = "";
+})
+
+
 // a refference on the current UserProfile
 const currentProfile = profiles.find(profile => profile.userID === loggedInProfileID);
+
+// the chatId, of th ecurrently opened chat
+let selectedChatID = 0;
 
 console.log(currentProfile);
 renderChatSelection();
@@ -20,6 +35,7 @@ function renderChatSelection() {
     document.querySelector(`#chatID_${currentChatID}`).addEventListener("click", function () {
       // print the history into the html
       document.querySelector(`#chatHistory`).innerHTML = chats.find(chat => chat.chatID === currentChatID).writeHistoryHtml();
+      selectedChatID = currentChatID;
     })
   });
 }

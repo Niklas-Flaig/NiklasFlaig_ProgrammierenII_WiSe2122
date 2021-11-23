da6965149c825ac86c577f4614c654a236617314
Niklas-Flaig <niklasjensflaig@gmail.com>
2021-11-23 15:43:11 +0100

    removed the former way of adding a new TextMessage to the Chats history

--- javaScripts/script.js
+++ javaScripts/script.js
@@ -1,25 +1,3 @@
-document.querySelector("#sendMessage_Button").addEventListener("click", function () {
-  // get the written Text
-  const messageText = document.querySelector("#textMessage_TextInput").value;
-  // add a new TextMessage to the current Chats history
-  chats.find(chat => chat.chatID === selectedChatID).addToHistory(new TextMessage(loggedInProfileID, messageText));
-  // re-render the chatHistory and clear the input field
-  document.querySelector(`#chatHistory`).innerHTML = chats.find(chat => chat.chatID === selectedChatID).writeHistoryHtml();
-  document.querySelector("#textMessage_TextInput").value = "";
-})
-document.querySelector("#textMessage_TextInput").addEventListener("keypress", function (e) {
-  if (e.key === "Enter") {
-    // get the written Text
-    const messageText = document.querySelector("#textMessage_TextInput").value;
-    // add a new TextMessage to the current Chats history
-    chats.find(chat => chat.chatID === selectedChatID).addToHistory(new TextMessage(loggedInProfileID, messageText));
-    // re-render the chatHistory and clear the input field
-    document.querySelector(`#chatHistory`).innerHTML = chats.find(chat => chat.chatID === selectedChatID).writeHistoryHtml();
-    document.querySelector("#textMessage_TextInput").value = "";
-  }
-});
-
-
 // a refference on the current UserProfile
 const currentProfile = profiles.find(profile => profile.userID === loggedInProfileID);
 

9e79ecbeec756ffb51bb2159a875612ff4ff6cc1
Niklas-Flaig <niklasjensflaig@gmail.com>
2021-11-26 17:15:24 +0100

    created basic server app
    mostly copied the code from the socket.io lesson.

---
+++ server/server.js
@@ -0,0 +1,30 @@
+// Modules
+// Create a http-server with a request listener 
+var app = require('http').createServer(handler);
+// Create a socket.io instance and bind it to the http-server
+var io = require('socket.io')(app);
+// To access the file system
+var fs = require('fs');
+
+// Make the http-server listen on port 80
+app.listen(80);
+console.log( "Webserver is listening on Port 80" );
+
+// Serving the site - create a http-response
+function handler (req, res) {
+  fs.readFile(__dirname + '/../index.html',
+  function (err, data) {
+    if (err) {
+      res.writeHead(500);
+      return res.end('Error loading index.html');
+    }
+    res.writeHead(200);
+    res.end(data);
+  });
+}
+
+
+// listens for a new connection
+io.on('connection', function (socket) {
+  
+});

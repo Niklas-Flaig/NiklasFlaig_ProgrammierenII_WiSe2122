7df47538899dbba69c8fb464b99288c2fa4efbc3
Niklas-Flaig <niklasjensflaig@gmail.com>
2021-12-07 21:48:22 +0100

    moved the vue-stuff (clientSide) into an external script

---
+++ javaScripts/vue.js
@@ -0,0 +1,28 @@
+
+      let chatApp = new Vue({
+        el: "#chat",
+        data: {
+          // the clients userID
+          clientUserID: 01,
+          // the own Profile of the client
+          clientProfile: {},
+          // the chats this user participates in
+          clientChats: [],
+          
+          
+          currentChatID: 0,
+          newMessageText: "",
+          
+          chatHistory: "",
+        },
+        methods: {
+          switchChat: function (thisChatID) {
+            this.chatHistory = this.clientChats.find(chat => chat.chatID === thisChatID).getHistory();
+            this.currentChatID = thisChatID;
+          },
+          addMessageToHistory: function () {
+            this.clientChats.find(chat => chat.chatID === this.currentChatID).addToHistory(new TextMessage(loggedInProfileID, this.newMessageText));
+            this.newMessageText = "";
+          }
+        }
+      });
